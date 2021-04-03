c453e7cf0f9155b698cc2543e7b5cb78ea37c45b
Darhan2020 <darhan1976@gmail.com>
2021-04-03 14:56:41 -0400

    Home work 04.03

---
+++ CodeWars/HW04.03.js
@@ -0,0 +1,23 @@
+class Calculator{
+    constructor(x,y){
+    this.x = x;
+    this.y = y;
+    }
+    get sum(){
+    return this.x + this.y;
+    }
+    get diff(){
+    return this.x - this.y;
+    }
+    get mult(){
+    return this.x * this.y;
+    }
+    get div(){
+      return this.x / this.y;
+    }  
+    }
+    let myCalculator = new Calculator(8,4);
+    console.log(myCalculator.sum);
+    console.log(myCalculator.diff);
+    console.log(myCalculator.mult);
+    console.log(myCalculator.div);
