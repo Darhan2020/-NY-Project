5ae40a44bcd159d7380d06fb6ee47a7aed6c193b
Darhan2020 <darhan1976@gmail.com>
2021-04-25 20:28:56 -0400

    PU04.25.03

--- Automation/textboxPracticeForm.js
+++ Automation/textboxPracticeForm.js
@@ -1,37 +1,37 @@
 class formTextBox {
-    get labelHeader(){
-      return $$("#app > div > div > div.pattern-backgound.playgound-header > div")[0].getText();
-    }
-    get labelName() {
-      return $("#userName-label").getText();
-    }
-    get labelEmail() {
-      return $("#userEmail-label").getText();
-    }
-    get labelGender() {
-      return $$("#genterWrapper > div.col-md-3.col-sm-12")[0].getText();
-    }
-    get labelMobileNumber() {
-      return $("#userNumber-label").getText();
-    }
-    get labelDateOfBirth() {
-      return $("#dateOfBirth-label").getText();
-    }
-    get labelSubjects() {
-      return $$("#subjects-label")[0].getText();
-    }
-    get labelHobbies() {
-      return $$("#subjects-label")[1].getText();
-    }
-    get labelPicture() {
-      return $$("#subjects-label")[2].getText();
-    }
-    get labelCurrentAddress() {
-      return $("#currentAddress-label").getText();
-    }
-    get labelStateAndCity() {
-      return $("#stateCity-label").getText();
-    
-    }  
+  get labelHeader(){
+    return $$("#app > div > div > div.pattern-backgound.playgound-header > div")[0].getText();
+  }
+  get labelName() {
+    return $("#userName-label").getText();
+  }
+  get labelEmail() {
+    return $("#userEmail-label").getText();
+  }
+  get labelGender() {
+    return $$("#genterWrapper > div.col-md-3.col-sm-12")[0].getText();
+  }
+  get labelMobileNumber() {
+    return $("#userNumber-label").getText();
+  }
+  get labelDateOfBirth() {
+    return $("#dateOfBirth-label").getText();
+  }
+  get labelSubjects() {
+    return $$("#subjects-label")[0].getText();
+  }
+  get labelHobbies() {
+    return $$("#subjects-label")[1].getText();
+  }
+  get labelPicture() {
+    return $$("#subjects-label")[2].getText();
+  }
+  get labelCurrentAddress() {
+    return $("#currentAddress-label").getText();
+  }
+  get labelStateAndCity() {
+    return $("#stateCity-label").getText();
+  
+  }  
 }
-    export default new formTextBox();
+  export default new formTextBox();
